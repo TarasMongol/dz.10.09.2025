b53ffc9c03504ba2856125651996ffdc75b8da23
Retep <Retep@DESKTOP-K8SA48L>
2025-09-10 23:17:24 +0300

    Добавьте файлы проекта.

---
+++ dz.10.09.2025/FileName.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Deter {
+private:
+    int numerat;
+    int denom;
+
+public:
+    void Init(int number, int den) {
+        numerat = number;
+        denom = den;
+    }
+
+    void Print() {
+        cout << numerat << "/" << denom << endl;
+    }
+
+    void SetNumerat(int num) {
+        numerat = num;
+    }
+
+    int GetNumerat() {
+        return numerat;
+    }
+
+    void SetDenom(int denom) {
+        denom = denom;
+    }
+
+    int GetDenom() {
+        return denom;
+    }
+};
+
+int main() {
+    Deter f;
+
+    f.Init(3, 4);
+    f.Print();
+
+    f.SetNumerat(5);
+    f.Print();
+
+    int d = f.GetDenom();
+    cout << "denom:  " << d << endl;
+}
